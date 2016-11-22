cf263f882fa42d513bfe9a9206df26aa81607685
darwinmorales <darwin.morales.tech@gmail.com>
2016-11-23 08:00:09 +1100

    playing with props

--- lessons/05-owner-ownee/App.js
+++ lessons/05-owner-ownee/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Widget = (props) => {
     return (
         <div>
-            <input type="text" onChange={props.update}/>
+            <input type="text" onChange={props.update} value={props.txt}/>
             <h1>{props.txt}</h1>
             <h1>{props.name}</h1>
         </div>
@@ -24,16 +24,35 @@ class App extends React.Component {
         this.setState({txt: e.target.value})
     }
 
+    componentDidMount(){
+        this.setState({
+            txt: this.props.txt
+        })
+    }
+
     render() {
         return (
             <div>
-                <Widget txt={this.state.txt} name='hello world' update={this.update}/>
+                <Widget txt={this.state.txt} name={this.props.name} update={this.update}/>
                 <Widget txt={this.state.txt} update={this.update}/>
                 <Widget txt={this.state.txt} update={this.update}/>
                 <Widget txt={this.state.txt} update={this.update}/>
             </div>
         );
     }
+}
+
+
+App.propTypes = {
+    txt: React.PropTypes.string,
+    name: React.PropTypes.string
+
+}
+
+App.defaultProps = {
+    txt: 'this is the default txt',
+    name: 'hello world'
+
 }
 
 
