6621f277c8dd1a4109c8362691fe0f54a4b6517e
darwinmorales <darwin.morales.tech@gmail.com>
2016-11-24 14:10:55 +1100

    playing with props

--- lessons/06-refs/App.js
+++ lessons/06-refs/App.js
@@ -3,50 +3,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
+    constructor() {
+        super();
+        this.state = {
+            red: 0,
+            green: 0,
+            blue: 0,
+            name: ""
+        }
+        this.update = this.update.bind(this)
+        this.nameUpdate = this.nameUpdate.bind(this)
     }
-    this.update = this.update.bind(this)
-  }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    })
-  }
-  render(){
-    return (
-      <div>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br />
-      </div>
-    );
-  }
+
+    update(e) {
+        this.setState({
+            red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+            green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+            blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+        })
+    }
+
+    nameUpdate(e) {
+        this.setState({
+            name: ReactDOM.findDOMNode(this.refs.name.refs.inp).value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Slider ref="red" update={this.update}/>
+                {this.state.red}
+                <br />
+                <Slider ref="green" update={this.update}/>
+                {this.state.green}
+                <br />
+                <Slider ref="blue" update={this.update}/>
+                {this.state.blue}
+                <br />
+                <input ref="name" type="text" onChange={this.nameUpdate} value={this.state.name}/>
+                <br />
+            </div>
+        );
+    }
 }
 
 class Slider extends React.Component {
-  render(){
-    return (
-        <div>
-        <input ref="inp" type="range"
-          min="0"
-          max="255"
-          onChange={this.props.update} />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <input ref="inp" type="range"
+                       min="0"
+                       max="255"
+                       onChange={this.props.update}/>
+            </div>
+        );
+    }
 }
 
 
