79e858c4e0e91a81f24461a56e5a5a0a854307bd
darwinmorales <darwin.morales.tech@gmail.com>
2016-11-28 22:17:18 +1100

    playing with Components

--- lessons/07-child-properties/App.js
+++ lessons/07-child-properties/App.js
@@ -2,17 +2,22 @@
 
 import React from 'react';
 class App extends React.Component {
-  render(){
-    return <Button>I <Heart/> React</Button>
-  }
+    render() {
+        return (
+        <div>
+            <Button>I <Heart/> React</Button>
+            <Button>I <Dollar/> React</Button>
+        </div>)
+    }
 }
 
 class Button extends React.Component {
-  render(){
-    return <button>{this.props.children}</button>
-  }
+    render() {
+        return <button>{this.props.children}</button>
+    }
 }
 
 const Heart = () => <span className="glyphicon glyphicon-heart"></span>
+const Dollar = () => <span className="glyphicon glyphicon-dollar"></span>
 
 export default App
