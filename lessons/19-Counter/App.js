427ded470157fc08449ec3f87f49e983fff668b2
dmorales <dmorales@infomedia.com.au>
2016-12-02 08:41:17 +1100

    Counter app redux-style

--- lessons/19-Counter/App.js
+++ lessons/19-Counter/App.js
@@ -1,35 +1,49 @@
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// const Counter = ({
+//     value,
+//     onIncrement,
+//     onDecrement
+// }) => (
+//     <div>
+//         <h1>{value}</h1>
+//         <button onClick={onIncrement}>+</button>
+//         <button onClick={onDecrement}>-</button>
+//     </div>
+// );
+//
+// const render = () => {
+//     ReactDOM.render(
+//         <Counter
+//             value={store.getState()}
+//             onIncrement={() =>
+//                 store.dispatch({
+//                     type: 'INCREMENT'
+//                 })
+//             }
+//             onDecrement={() =>
+//                 store.dispatch({
+//                     type: 'DECREMENT'
+//                 })
+//             }
+//         />,
+//         document.getElementById('root')
+//     );
+// }
+
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-
-const Counter = ({
-    value,
-    onIncrement,
-    onDecrement
-}) => (
-    <div>
-        <h1>{value}</h1>
-        <button onClick={onIncrement}>+</button>
-        <button onClick={onDecrement}>-</button>
-    </div>
-);
-
-const render = () => {
-    ReactDOM.render(
-        <Counter
-            value={store.getState()}
-            onIncrement={() =>
-                store.dispatch({
-                    type: 'INCREMENT'
-                })
-            }
-            onDecrement={() =>
-                store.dispatch({
-                    type: 'DECREMENT'
-                })
-            }
-        />,
-        document.getElementById('root')
-    );
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <b>Bold</b>
+            </div>
+        );
+    }
 }
+
+export default App
