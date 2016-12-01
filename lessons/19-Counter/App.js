813c590f34d4579d3058e5482470f874db7f0310
dmorales <dmorales@infomedia.com.au>
2016-12-02 08:54:06 +1100

    Counter app redux-style

--- lessons/19-Counter/App.js
+++ lessons/19-Counter/App.js
@@ -1,18 +1,17 @@
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 //
-// const Counter = ({
-//     value,
-//     onIncrement,
-//     onDecrement
-// }) => (
-//     <div>
-//         <h1>{value}</h1>
-//         <button onClick={onIncrement}>+</button>
-//         <button onClick={onDecrement}>-</button>
-//     </div>
-// );
+const Counter = ({
+    value,
+    onIncrement,
+    onDecrement
+}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
+);
 //
 // const render = () => {
 //     ReactDOM.render(
@@ -33,14 +32,24 @@
 //     );
 // }
 
-import React from 'react';
 
 class App extends React.Component {
-    render(){
+    render() {
         return (
             <div>
-                <h1>Hello World</h1>
-                <b>Bold</b>
+                <Counter
+                    value={store.getState()}
+                    onIncrement={() =>
+                                store.dispatch({
+                                    type: 'INCREMENT'
+                                })
+                            }
+                    onDecrement={() =>
+                                store.dispatch({
+                                    type: 'DECREMENT'
+                                })
+                            }
+                />,
             </div>
         );
     }
