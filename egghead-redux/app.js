824c6c5ad4da3e313af1ad7fb3388a14a805375f
darwinmorales <darwin.morales.tech@gmail.com>
2016-12-02 23:53:01 +1100

    playing with redux

--- egghead-redux/app.js
+++ egghead-redux/app.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class HelloWorldComponent extends React.Component {
-    render() {
-        return (
-            <h1>Hello {this.props.name}</h1>
-        );
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
     }
 }
 
-React.render(
-    <HelloWorldComponent name="Joe Schmoe"/>,
-    document.getElementById('react_example')
+const {createStore} = Redux;
+const store = createStore(counter)
+
+const Counter = ({
+    value,
+    onIncrement,
+    onDecrement
+}) => (
+    <div>
+        <h1>{value}</h1>
+        <button onClick={onIncrement}>+</button>
+        <button onClick={onDecrement}>-</button>
+    </div>
 );
+
+const render = () => {
+    ReactDOM.render(
+        <Counter
+            value={store.getState()}
+            onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+            onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+        />,
+        document.getElementById('root')
+    )
+}
+
+render()
+store.subscribe(render)
