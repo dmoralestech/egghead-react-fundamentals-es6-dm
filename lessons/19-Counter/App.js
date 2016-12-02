5e108df8d49501eec63a6e4dfe3e60792df01e45
darwinmorales <darwin.morales.tech@gmail.com>
2016-12-02 23:36:30 +1100

    playing with redux

--- lessons/19-Counter/App.js
+++ lessons/19-Counter/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { createStore } from 'redux';
 
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
 const store = createStore(counter);
 
 //
-const Counter = ({value, onIncrement, onDecrement}) =>
+const Counter1 = ({value, onIncrement, onDecrement}) =>
     (<div>
         <h1>{value}</h1>
         <button onClick={onIncrement}>+</button>
@@ -35,7 +46,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Counter
+                <Counter1
                     value={store.getState()}
                     onIncrement={() =>
                                 store.dispatch({
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,53 @@
 var port = process.env.PORT || 3000;
 
 var lessons = [
-  "01-intro",
-  "02-render",
-  "03-properties",
-  "04-state",
-  "05-owner-ownee",
-  "06-refs",
-  "07-child-properties",
-  "08-lifecycle-mounting",
-  "09-lifecycle-mounting-usage",
-  "10-lifecycle-updates",
-  "11-higher-order",
-  "12-composable",
-  "13-dynamic",
-  "14-build-compiler",
-  "17-dmTest",
-    "18-leaderboard"
+    "01-intro",
+    "02-render",
+    "03-properties",
+    "04-state",
+    "05-owner-ownee",
+    "06-refs",
+    "07-child-properties",
+    "08-lifecycle-mounting",
+    "09-lifecycle-mounting-usage",
+    "10-lifecycle-updates",
+    "11-higher-order",
+    "12-composable",
+    "13-dynamic",
+    "14-build-compiler",
+    "17-dmTest",
+    "18-leaderboard",
+    "19-Counter"
 ]
 
 var entry = {};
 
-lessons.forEach(function(lesson){
-  entry[lesson] = './lessons/' + lesson + '/main.js'
+lessons.forEach(function (lesson) {
+    entry[lesson] = './lessons/' + lesson + '/main.js'
 })
 
 module.exports = {
-  entry: entry,
-  output: {
-    path: './lessons/',
-    filename: "[name]/index.js",
-    publicPath: '/'
-  },
-  devServer: {
-    inline: true,
-    contentBase: './lessons/',
-    port: port
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react', 'redux']
-        }
-      }
-    ]
-  }
+    entry: entry,
+    output: {
+        path: './lessons/',
+        filename: "[name]/index.js",
+        publicPath: '/'
+    },
+    devServer: {
+        inline: true,
+        contentBase: './lessons/',
+        port: port
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
 };
