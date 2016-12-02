db645524c059daadfe35f22c47cf6731395c5fda
darwinmorales <darwin.morales.tech@gmail.com>
2016-12-03 06:18:49 +1100

    playing with redux

--- lessons/19-Counter/App.js
+++ lessons/19-Counter/App.js
@@ -14,33 +14,12 @@ const counter = (state = 0, action) => {
 
 const store = createStore(counter);
 
-//
 const CounterComponent = ({value, onIncrement, onDecrement}) =>
     (<div>
         <h1>{value}</h1>
         <button onClick={onIncrement}>+</button>
         <button onClick={onDecrement}>-</button>
     </div>);
-//
-// const render = () => {
-//     ReactDOM.render(
-//         <Counter
-//             value={store.getState()}
-//             onIncrement={() =>
-//                 store.dispatch({
-//                     type: 'INCREMENT'
-//                 })
-//             }
-//             onDecrement={() =>
-//                 store.dispatch({
-//                     type: 'DECREMENT'
-//                 })
-//             }
-//         />,
-//         document.getElementById('root')
-//     );
-// }
-
 
 class App extends React.Component {
     render() {
