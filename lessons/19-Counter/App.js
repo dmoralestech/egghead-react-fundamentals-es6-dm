84cffe6f10b045d5733c9cf0a7a5ee4ea68ad405
darwinmorales <darwin.morales.tech@gmail.com>
2016-12-03 17:33:06 +1100

    playing with redux

--- lessons/19-Counter/App.js
+++ lessons/19-Counter/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createStore} from 'redux';
+// import {createStore} from 'redux';
 
 const counter = (state = 0, action) => {
     console.log("enter counter");
@@ -13,6 +13,30 @@ const counter = (state = 0, action) => {
             return state;
     }
 }
+
+const createStore = (reducer) => {
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    };
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        }
+    };
+
+    dispatch({}); // dummy dispatch
+
+    return { getState, dispatch, subscribe };
+
+};
 
 const store = createStore(counter);
 
