0316ba4775644a9138aecfa66fa1ac2fedef6b72
darwinmorales <darwin.morales.tech@gmail.com>
2016-12-14 23:28:50 +1100

    playing with redux

--- lessons/20-Todos/Box.js
+++ lessons/20-Todos/Box.js
@@ -1,7 +1,7 @@
 const Box = x => ({
     map: f => Box(f(x)),
     fold: f => f(x),
-    inspect: () => `Box($(x))`
+    inspect: () => `Box(${x})`
 })
 
 const percentToFloat = str =>
@@ -11,10 +11,28 @@ const percentToFloat = str =>
 
 const moneyToFloat = str =>
     Box(str)
-    .map( s => s.replace(/\$/g, ''))
-    .map(r => parseFloat(r))
+        .map(s => s.replace(/\$/g, ''))
+        .map(r => parseFloat(r))
 
 const applyDiscount = (price, discount) =>
     moneyToFloat(price)
-        .map(cost => percentToFloat(discount)
-                        .map(savings => cost - cost * savings))
+        .fold(cost => percentToFloat(discount)
+            .fold(savings => cost - cost * savings))
+
+const Right = x => ({
+    map: f => Right(f(x)),
+    fold: (f, g) => g(x),
+    inspect: () => `Right(${x})`
+})
+
+const Left = x => ({
+    map: f => Left(x),
+    fold: (f, g) => f(x),
+    inspect: () => `Left(${x})`
+})
+
+const result = Left(2).map(x => x + 1).map(x => x / 2).fold(x=> 'error', x => x)
+
+const Either = Right || Left
+
+
